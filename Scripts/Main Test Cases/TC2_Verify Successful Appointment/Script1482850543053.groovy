6e5acbfaa38a1436d8a0966cd619ea9e627b71fc
Alex <dev.alex.88@gmail.com>
2020-09-24 16:44:13 +0700

    update

--- Scripts/Main Test Cases/TC2_Verify Successful Appointment/Script1482850543053.groovy
+++ Scripts/Main Test Cases/TC2_Verify Successful Appointment/Script1482850543053.groovy
@@ -25,44 +25,43 @@ WebUI.comment('Given that the user has logged into their account')
 
 WebUI.openBrowser(GlobalVariable.G_SiteURL)
 
-WebUI.callTestCase(findTestCase('Common Test Cases/Login'), [('Username') : 'John Doe', ('Password') : 'ThisIsNotAPassword'], 
-    FailureHandling.STOP_ON_FAILURE)
+WebUI.takeScreenshotAsCheckpoint('login', null)
+
+WebUI.callTestCase(findTestCase('Common Test Cases/Login'), [('Username') : 'John Doe', ('Password') : 'ThisIsNotAPassword'], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.comment('And Appointment page is displayed')
 
-if (true) {
-    WebUI.selectOptionByLabel(findTestObject('Page_CuraAppointment/lst_Facility'), 'Hongkong CURA Healthcare Center', false)
+WebUI.takeScreenshotAsCheckpoint('appointment', null)
 
-    WebUI.check(findTestObject('Page_CuraAppointment/chk_Medicaid'))
+WebUI.selectOptionByLabel(findTestObject('Page_CuraAppointment/lst_Facility'), 'Hongkong CURA Healthcare Center', false)
 
-    WebUI.check(findTestObject('Page_CuraAppointment/chk_Readmission'))
+WebUI.check(findTestObject('Page_CuraAppointment/chk_Medicaid'))
 
-    WebUI.setText(findTestObject('Page_CuraAppointment/txt_VisitDate'), '27/12/2016')
+WebUI.check(findTestObject('Page_CuraAppointment/chk_Readmission'))
 
-    WebUI.setText(findTestObject('Page_CuraAppointment/txt_Comment'), 'Please make appointment as soon as possible.')
-}
+WebUI.setText(findTestObject('Page_CuraAppointment/txt_VisitDate'), '27/12/2016')
+
+WebUI.setText(findTestObject('Page_CuraAppointment/txt_Comment'), 'Please make appointment as soon as possible.')
 
 WebUI.comment('When he fills in valid information in Appointment page')
 
 WebUI.click(findTestObject('Page_CuraAppointment/btn_BookAppointment'))
 
+WebUI.takeScreenshotAsCheckpoint('confirmation', null)
+
 WebUI.verifyTextPresent('Appointment Confirmation', false)
 
 WebUI.comment('Then he should be able to book a new appointment')
 
-if (true) {
-    WebUI.verifyMatch('Hongkong CURA Healthcare Center', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Facility')), 
-        false)
+WebUI.verifyMatch('Hongkong CURA Healthcare Center', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Facility')), false)
 
-    WebUI.verifyMatch('Yes', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_HospitalReadmission')), false)
+WebUI.verifyMatch('Yes', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_HospitalReadmission')), false)
 
-    WebUI.verifyMatch('Medicaid', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Program')), false)
+WebUI.verifyMatch('Medicaid', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Program')), false)
 
-    WebUI.verifyMatch('27/12/2016', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_VisitDate')), false)
+WebUI.verifyMatch('27/12/2016', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_VisitDate')), false)
 
-    WebUI.verifyMatch('Please make appointment as soon as possible.', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Comment')), 
-        false)
-}
+WebUI.verifyMatch('Please make appointment as soon as possible.', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Comment')), false)
 
 WebUI.takeScreenshot()
 WebUI.closeBrowser()
